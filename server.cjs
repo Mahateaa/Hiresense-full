de8cc47d76772f79ee122784e6fe91f9d551f931
Mahathi <mahathi.nagalapuram@gmail.com>
2026-04-07 02:25:26 +0530

    fix express wildcard route

--- server.cjs
+++ server.cjs
@@ -222,7 +222,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 const PORT = process.env.PORT || 5000;
